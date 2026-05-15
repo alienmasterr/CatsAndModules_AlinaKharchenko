36bf35b16615916697ce011b50cc06bc7622c3d2
alina <alina230605@gmail.com>
2026-05-15 13:12:20 +0300

    Initial Commit

---
+++ CatsAndModules_AlinaKharchenko/CatsAndModules_AlinaKharchenkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndModules_AlinaKharchenkoApp.swift
+//  CatsAndModules_AlinaKharchenko
+//
+//  Created by alina on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndModules_AlinaKharchenkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsAndModules_AlinaKharchenko/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatsAndModules_AlinaKharchenko
+//
+//  Created by alina on 15.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
